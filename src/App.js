61598bf13380ea4c9dbf80dbcc27230fcf3d3529
timofei <timofei.chaplinskiy@gmail.com>
2019-08-22 23:16:38 +0300

    add template

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import Museum from "./museum/page";
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const a = "123as";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Museum hello={a}/>
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <React.Fragment>
+      <Museum hello={a}/>
+    </React.Fragment>
   );
 }
 
